1e42c466210de4a21ba1ec4b4ab19d613d1edd1e
Todor Genov <todor@subnet.co.za>
2012-10-13 22:22:59 +0200

    Registrant update template

---
+++ erb/update_domain_registrant.rb
@@ -0,0 +1,13 @@
+<epp:epp xmlns:epp="urn:ietf:params:xml:ns:epp-1.0" xmlns:domain=
+"urn:ietf:params:xml:ns:domain-1.0">
+  <epp:command>
+    <epp:update>
+      <domain:update>
+        <domain:name><%= domainName %></domain:name>
+        <domain:chg>
+          <domain:registrant><%= registrant %></domain:registrant>
+        </domain:chg>
+      </domain:update>
+    </epp:update>
+  </epp:command>
+</epp:epp>
