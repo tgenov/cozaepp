e1d1aac78ddea0ef3293c83515e514a7a3ba565b
Todor Genov <todor@subnet.co.za>
2012-10-13 18:16:03 +0200

    XML erb temlates and XSD validators

--- lib/cozaepp.rb
+++ lib/cozaepp.rb
@@ -1,5 +1,31 @@
+require "socket"
+require "securerandom"
+require "erb"
+require "epp"
 require "cozaepp/version"
 
-module Cozaepp
-  # Your code goes here...
+module CozaEPP
+  class Client
+    def initialize(server, tag, password, port = 3121)  
+      raise ArgumentError unless server and tag and password
+      @gemRoot = Gem::Specification.find_by_name("cozaepp").gem_dir
+      @eppTag = tag
+      @eppPassword = password
+      @epp = Epp::Server.new(
+        :server => server,
+        :port => port,
+        :tag => tag,
+        :password => password
+      )
+      @epp.open_connection
+      self.login
+    end
+
+    def login
+      xml = ERB.new(File.read(@gemRoot + "/erb/login.erb")).result
+      puts xml
+    end
+  
+  end
 end
+
